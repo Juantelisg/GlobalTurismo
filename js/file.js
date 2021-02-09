ad7d2809e9044dab6a88b7fce295cfe2c6c7bb13
Juan Telis <Juantelis03@gmail.com>
2021-02-09 03:09:10 -0300

    commit de entrega 9 EVENTOS

--- js/file.js
+++ js/file.js
@@ -1,61 +1,60 @@
 document.getElementById("calcular").onclick= comprar;
-document.getElementById("Consultar otros").onclick= consultar;
+document.getElementById("calcularCompra").onclick= calcular;
+document.getElementById("evento").onclick= evento;
 
-
-
-
-function comprar() {
-
-    var alemania = {destino: "berlin", costo: 1000};
-    var brasil = {destino: "rio de janeiro", costo: 1400};
-    var puntaC = {destino: "punta cana", costo: 1200};
-    var colombia = {destino: "bogota", costo: 1200};
-    var margarita = {destino: "margarita", costo: 1000};
-    var losRoques = {destino: "los roques", costo: 1200};
+function calcular() {
+    
+    var alemania = {destino: "berlin", costo: 900};
+    var brasil = {destino: "rio de janeiro", costo: 500};
+    var puntaC = {destino: "punta cana", costo: 600};
+    var colombia = {destino: "bogota", costo: 400};
+    var margarita = {destino: "margarita", costo: 600};
+    var losRoques = {destino: "los roques", costo: 1000};
+    
+    var destino = document.getElementById("destino").value;
+    var cantidadP = document.getElementById("cantidadPersonas").value;
+    
    
-    var destino = prompt("Cual es tu destino:").toLowerCase();
-    
-    
+
     if (destino === alemania.destino) {
-        var personas = prompt("Cuantas personas seran:");
-        alert("El precio total del viaje es " + "$" + alemania.costo * personas + "USD");
-        tipoPago()
+        alert(`El precio total del viaje es $ ${alemania.costo * cantidadP} USD`);
     }
     else if (destino === brasil.destino) {
-        var personas = prompt("Cuantas personas van:");
-        alert("El precio total del viaje es " + "$" + brasil.costo * personas + "USD");
-        tipoPago() 
+        alert(`El precio total del viaje es $ ${brasil.costo * cantidadP} USD`); 
     }
     else if (destino === puntaC.destino) {
-        var personas = prompt("Cuantas personas van:");
-        alert("El precio total del viaje es " + "$" + puntaC.costo * personas + "USD");
-        tipoPago() 
+        alert(`El precio total del viaje es $ ${puntaC.costo * cantidadP} USD`); 
     }
     else if (destino === colombia.destino) {
-        var personas = prompt("Cuantas personas van:");
-        alert("El precio total del viaje es " + "$" + colombia.costo * personas + "USD");
-        tipoPago() 
+        alert(`El precio total del viaje es $ ${colombia.costo * cantidadP} USD`); 
     }
     else if (destino === margarita.destino) {
-        var personas = prompt("Cuantas personas van:");
-        alert("El precio total del viaje es " + "$" + margarita.costo * personas + "USD");
-        tipoPago() 
+        alert(`El precio total del viaje es $ ${margarita.costo * cantidadP} USD`); 
     }
     else if (destino === losRoques.destino) {
-        var personas = prompt("Cuantas personas van:");
-        alert("El precio total del viaje es " + "$" + losRoques.costo * personas + "USD");
-        tipoPago() 
+        alert(`El precio total del viaje es $ ${losRoques.costo * cantidadP} USD`); 
     }
     else {
         alert("PUEDE QUE AUN NO TENGAMOS VIAJES DISPONIBLES A ESE DESTINO!")
-    }
+    };
 
     
-};
+}
+
 
 function paquete(destino, costo) {
     this.destino = destino;
     this.costo = costo;
+}
+
+function evento(event) {
+
+    if (event.keyCode == 13) {
+        alert("Has presionado el ENTER")
+    }
+    else{
+        alert('Has presionado una letra dentro de este campo');
+    }
 }
 
 function tipoPago() {
@@ -76,36 +75,5 @@ function tipoPago() {
 
 
 
-/* function consultar() {
-
-    var paises = ["Argentina", "Brasil", "Colombia", "Mexico", "Republica Dominicana", "Francia", "Alemania", "Inglaterra", "Taiwan", "Tailandia", "Japon"];
-    var latinoAmetica = paises.slice(0, 5);
-    var europa = paises.slice(5, 8);
-    var asia = paises.slice(8, 11);
-
-    var viaje = new paquete("Mexico", 900);
-    var viaje2 = new paquete("Argentina", 900);
-    var viaje3 = new paquete("Venezuela", 800);
-
-
-    var consulta = prompt("Que paquete deseas consultar? -> 1) Latino America | 2) Europa | 3) Asia");
-
-    if (consulta == 1) {
-        alert("Los destinos a Latino America disponibles son: " + latinoAmetica);
-    }
-    else if (consulta == 2) {
-        alert("Los destinos a Europa disponibles son: " + europa);
-    }
-    else if (consulta == 3) {
-        alert("Los destinos a Asia disponibles son: " + asia);
-    }
-    
-    else{
-        alert("No contamos con la disponibilidad de ese paquete");
-    }
-};
-
-
-//destinos// */
 
 
